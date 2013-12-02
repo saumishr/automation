9616926f0849294dac4b890fdbcfde4d7e08c8fb
saumishr <saumishr@www.domain1.com>
2013-12-02 17:47:54 +0530

    Next version of automation scripts

--- create_categories.py
+++ create_categories.py
@@ -11,49 +11,65 @@ if __name__ == "__main__":
 	from mezzanine.blog.models import BlogParentCategory, BlogCategory
 	from django.template.defaultfilters import slugify
 
-	workbook = xlrd.open_workbook('xls/Wishradio - Categories.xlsx')
+	CATEGORIES_XLS_PATH = 'assets/categories/xls/Wishradio - Categories.xlsx'
+	PARENT_CATEGORY_INDEX = 1
+	SUB_CATEGORIES_INDEX = 2
+
+	workbook = xlrd.open_workbook(CATEGORIES_XLS_PATH)
 
 	def create_categories(sheet, start_index, end_index):
+		"""
+			First row entry of every section will contain parent_category name.
+		"""
 		categories = sheet.row_values(start_index)
 		parent_category = None
-		parent_category_list = BlogParentCategory.objects.filter(title=categories[1])
+		parent_category_list = BlogParentCategory.objects.filter(title=categories[PARENT_CATEGORY_INDEX])
 		if len(parent_category_list) == 0:
-			parent_category = BlogParentCategory(title=categories[1], slug=slugify(categories[1]))
+			parent_category = BlogParentCategory(slug=slugify(categories[PARENT_CATEGORY_INDEX]))
+			parent_category.title = categories[PARENT_CATEGORY_INDEX]
 			parent_category.save()
 		else:
 			parent_category = parent_category_list[0]
 
-		sub_categories = sheet.col_values(2, start_rowx=start_index, end_rowx=end_index)
+		sub_categories = sheet.col_values(SUB_CATEGORIES_INDEX, start_rowx=start_index, end_rowx=end_index)
+
 		for sub_category in sub_categories:
 			sub_category_list = BlogCategory.objects.filter(title=sub_category)
 			if len(sub_category_list) == 0:
-				sub_category = BlogCategory(title=sub_category, parent_category=parent_category, slug=slugify(sub_category))
-				sub_category.save()
+				sub_category_obj = BlogCategory(parent_category=parent_category, slug=slugify(sub_category))
+				sub_category_obj.title = sub_category 
+				sub_category_obj.save()
 			else:
-				sub_category = sub_category_list[0]
-				if sub_category.parent_category.title == parent_category.title:
+				sub_category_obj = sub_category_list[0]
+				if sub_category_obj.parent_category.title == parent_category.title:
 					print parent_category, " already has a sub category: ", sub_category
 				else:
-					sub_category.parent_category = parent_category
+					sub_category_obj.parent_category = parent_category
+				sub_category_obj.save()
 
 
-	for sheet in workbook.sheets():
-			column_0_values = sheet.col_values(colx=0)
+	sheets = workbook.sheets()
+	print '------------------------------------------------------------------------------------------'
+	print 'getting sheets...'
+	print 'sheets found: ', [sheet.name for sheet in sheets]
+	print '------------------------------------------------------------------------------------------'
+	for sheet in sheets:
+		column_0_values = sheet.col_values(colx=0)
 
-			start_tag = 1
-			end_tag = start_tag + 1
-			print "Processing sheet: ", sheet.name
+		start_tag = 1
+		end_tag = start_tag + 1
+		print "Processing sheet: ", sheet.name
 
-			while(start_tag in column_0_values):
-				start_index = column_0_values.index(start_tag)
-				if end_tag in column_0_values:
-					end_index = column_0_values.index(end_tag)
-					start_tag = end_tag
-					end_tag = start_tag + 1
-					create_categories(sheet, start_index, end_index )
-				else:
-					create_categories(sheet, start_index, sheet.nrows)
-					break
+		while(start_tag in column_0_values):
+			start_index = column_0_values.index(start_tag)
+			if end_tag in column_0_values:
+				end_index = column_0_values.index(end_tag)
+				create_categories(sheet, start_index, end_index )
+				start_tag = end_tag
+				end_tag = start_tag + 1
+			else:
+				create_categories(sheet, start_index, sheet.nrows)
+				break
 
 		
 
--- create_stores.py
+++ create_stores.py
@@ -3,6 +3,7 @@ import os
 import sys
 import xlrd
 import xlwt
+from os import walk
 
 if __name__ == "__main__":
 	# set your django setting module here
@@ -10,86 +11,286 @@ if __name__ == "__main__":
 	from django.contrib.auth.models import User
 	from userProfile.models import UserProfile
 	from mezzanine.blog.models import BlogPost, BlogParentCategory, BlogCategory
+	from django.template.defaultfilters import slugify
+	import uuid
+	from django.conf import settings
+	from django.core.files import File
 
-	workbook = xlrd.open_workbook('xls/1.Fashion.xls')
-	passbook = xlwt.Workbook()
-	passsheet = passbook.add_sheet('passwords')
-	passsheet.write(0, 0, 'username')
-	passsheet.write(0, 1, 'password')
+	MEDIA_URL = "static/media/"
+
+	# Absolute filesystem path to the directory that will hold user-uploaded files.
+	# Example: "/home/media/media.lawrence.com/media/"
+	MEDIA_ROOT = os.path.join(settings.PROJECT_ROOT, *MEDIA_URL.strip("/").split("/"))
+
+	"""
+	Global Configurations.
+	Please do not change these indexes. These are strictly as per the xls.
+	"""
+	XLS_CONTAINER 				= 'assets/xls_processed/'
+	LOGOS_CONTAINER 			= 'assets/logos/'
+	PASSWORD_FILE_PATH			= '/home/saumishr/passcodes/passwords.xls'
+	WEBSITE_URL_INDEX 			= 1
+	WEBSITE_NAME_INDEX 			= 2
+	EMAIL_INDEX 				= 3
+	LOGO_FILE_NAME_INDEX 		= 4
+	WISHRADIO_CATEGORY_INDEX 	= 8
+	STORE_DESCRIPTION_INDEX 	= 10
+	MAX_PASSWORD_CHARACTERS		= 5
+
+	store_count = 0
+
+	# def save_file_s3(file, path=''):
+	# 	''' Little helper to save a file
+	# 	'''
+	# 	filename = file._get_name()
+	# 	new_filename =  u'{name}.{ext}'.format(  name=uuid.uuid4().hex,
+	# 											ext=os.path.splitext(filename)[1].strip('.'))
+
+	# 	dir_path =  str(path)
+
+	# 	save_path = os.path.join(dir_path, new_filename)
+	# 	storage=S3BotoStorage(location=settings.STORAGE_ROOT)
+	# 	storage.save(save_path, file)
+
+	# 	return save_path
+
+	def save_file(file, path=''):
+		''' Little helper to save a file
+		'''
+		filename = file.name
+
+		# Enable this code to save the file with uid.
+		# new_filename =  u'{name}.{ext}'.format(	name=uuid.uuid4().hex,
+		# 										ext=os.path.splitext(filename)[1].strip('.'))
+		# else
+		new_filename = filename		
+
+		dir_path =  '%s/%s' % (MEDIA_ROOT, str(path))
+
+		if not os.path.exists(dir_path):
+			os.makedirs(dir_path)
+
+		save_path = os.path.join(dir_path, new_filename)
+
+		with open(save_path, 'wb+') as destination:
+			for chunk in file.chunks():
+				destination.write(chunk)
+
+		return os.path.join(path, new_filename)
+
+	def create_store(sheet, start_index, end_index, store_count, current_tag):
+		"""
+			Single item cloumns will have store details which are unique to them and will be used to create stores.
+			Also, these entries as expected to be in the first row of their respective row-range. 
+		"""
+		store_details 	= sheet.row_values(start_index)
+
+		url 			= store_details[WEBSITE_URL_INDEX]
+		store_name 		= store_details[WEBSITE_NAME_INDEX]
+		email 			= store_details[EMAIL_INDEX]
+		description 	= store_details[STORE_DESCRIPTION_INDEX]
+		logo_name 		= store_details[LOGO_FILE_NAME_INDEX]
+
+		print '------------------------------------------------------------------------------------------'
+		print 'Store: ', store_name
+		print '------------------------------------------------------------------------------------------'
+		print 'fetching store details...'
+		if logo_name:
+			"""
+				extract the file name & extension.
+			"""
+			logo 	= os.path.splitext(logo_name)[0]
+			ext 	= os.path.splitext(logo_name)[1]
 
 
-	def create_store(sheet, start_index, end_index, user_count):
-		store_details = sheet.row_values(start_index)
+			"""
+				Now start the dirty work. :(
+				Our contractor totally messed up the file names. This code is a corrective measure to clear up the mess.
+				This code is totally redudant once the logo names in the xls and logo file name are consistent.
+			"""
 
-		url = store_details[1]
-		store_name = store_details[2]
-		email = store_details[3]
-		description = store_details[10]
-		wr_categories = sheet.col_values(8, start_rowx=start_index, end_rowx=end_index)
+			"""
+				First issue: extra .png between filename & extension.
+			"""
+			orig_logo_name = str(current_tag)+'.' + logo + '.png' + ext
+
+
+			"""
+				Expected logo name.
+			"""
+			logo_name = str(current_tag)+'.' + logo + ext.lower()
+			path = logoFolder +  '/%s/%s' % (sheet.name, orig_logo_name)
+
+
+			"""
+				If path with extra .png in filename exists, remove the extra .png .
+			"""
+			if os.path.exists(path):
+				newpath = logoFolder +  '/%s/%s' % (sheet.name, logo_name) 
+				os.rename(path, newpath)
+			else:
+				"""
+					Second issue: ext name is in uppercase at some places.
+				"""
+				orig_logo_name = str(current_tag)+'.' + logo + '.png' + ext.upper()
+				path = logoFolder +  '/%s/%s' % (sheet.name, orig_logo_name)
+				if os.path.exists(path):
+					newpath = logoFolder +  '/%s/%s' % (sheet.name, logo_name)
+					os.rename(path, newpath)
+			"""
+				Rest all the issues are handled manually as they are not genric enough to be handled in automated script.
+			"""
+
+		wr_categories = sheet.col_values(WISHRADIO_CATEGORY_INDEX, start_rowx=start_index, end_rowx=end_index)
 		wr_categories = filter(None, wr_categories)
+		print 'categories: ', wr_categories
 
-		#Create the user first
+		"""
+			users will be created with default username: store_name + '_admin'
+		"""
 		username = store_name + '_admin'
 		user_list = User.objects.filter(username=username)
 		new_user = None
+
 		if len(user_list) == 0:
-			password = User.objects.make_random_password(5)
-			email = email.split()
-			print "username:", username, " email:", email[0], " password: ", password
-			new_user = User.objects.create_user(username, email[0], password)
+			print 'Creating profile for admin of store: ', store_name
+			password = User.objects.make_random_password(MAX_PASSWORD_CHARACTERS)
+			email = email.split()			
+			if len(email) > 0:
+				email = email[0]
+			else:
+				email = ''
+
+			print "username:", username, " email:", email, " password: ", password
+			new_user = User.objects.create_user(username, email, password)
 			new_user.first_name = username
 			new_user.save()
 			user_profile = UserProfile.objects.get(user=new_user)
 			user_profile.gender = 'male'
 			user_profile.save()
-			passsheet.write(user_count, 0, username)
-			passsheet.write(user_count, 1, password)
+			passsheet.write(store_count, 0, username)
+			passsheet.write(store_count, 1, password)
 			print "Created profile of ", username
 		else:
-			print "user ", username, " already exists..."
+			print "Admin for store: ", store_name, " already exists..."
 			new_user = user_list[0]
 
+		print 'Creating store: ', store_name
 		blog_post = None
 		blog_post_list = BlogPost.objects.filter(user=new_user)
 		if len(blog_post_list) == 0:
 			blog_post = BlogPost(web_url=url, content=description, title=store_name, user=new_user)
 			blog_post.save()
 		else:
+			print 'Store ', store_name, ' already exists...'
 			blog_post = blog_post_list[0]
 
+		print 'Adding categories...'
+		"""
+		ToDo: Need to check the existing list of categories with cateories mentioned in the xls.
+		Removed categories should also be updated in the blog_post object.
+		"""
 		for wr_parent_category in wr_categories:
 			parent_category_list = BlogParentCategory.objects.filter(title=wr_parent_category)
 			if len(parent_category_list) != 0:
 				parent_category = parent_category_list[0]
 				sub_categories = BlogCategory.objects.all().filter(parent_category=parent_category)
 				for sub_category in sub_categories:
-					if not BlogPost.objects.all().filter(categories=sub_category).exists():
+					if not blog_post.categories.all().filter(slug=slugify(sub_category)).exists():
 						blog_post.categories.add(sub_category)
 
+		print 'Attaching logo with the store...'				
+		if logo_name:
+			new_file_rel_path = 'users/store/%s/images/' % (blog_post.id)
+			path = logoFolder +  '/%s/%s' % (sheet.name, logo_name)
+			"""
+				Remove any spaces in the path.
+			"""
+			path = path.replace(" ", "")
+
+			featuredImageObj =  File(open(path, 'r'))
+			new_file_path = save_file(featuredImageObj, new_file_rel_path)
+			if blog_post.featured_image:
+				old_file_path = '%s/%s' % (MEDIA_ROOT, str(blog_post.featured_image.path))
+				if os.path.exists(old_file_path):
+					print 'Removed old logo...'
+					os.remove(old_file_path)
+
+			print 'new logo is set...'
+			blog_post.featured_image = new_file_path
 		blog_post.save()
 
+	"""
+		Main()
+	"""
+	f = []
+	for (dirpath, dirnames, filenames) in walk(XLS_CONTAINER):
+		f.extend(filenames)
+		break
 
-	user_count = 0
-	for sheet in workbook.sheets():
+	print '------------------------------------------------------------------------------------------'
+	print "Indexing files : ", f
+	print '------------------------------------------------------------------------------------------'
+
+	for filename in f:
+		print '------------------------------------------------------------------------------------------'
+		print 'started Processing file: ' + filename
+		print '------------------------------------------------------------------------------------------'
+
+		logoFolder = LOGOS_CONTAINER + os.path.splitext(filename)[0]
+		"""
+			'assets/xls' is supposed to contain all the xls files having store information.
+		"""
+		workbook = xlrd.open_workbook(XLS_CONTAINER + filename)
+
+		"""
+			passbook is the xls file generated in the end. This contains all the login credentials of the store owners.
+		"""
+		passbook = xlwt.Workbook()
+		passsheet = passbook.add_sheet('passwords')
+		passsheet.write(0, 0, 'username')
+		passsheet.write(0, 1, 'password')
+
+		sheets = workbook.sheets()
+		
+		print '------------------------------------------------------------------------------------------'
+		print 'getting sheets in ', filename
+		print 'sheets found: ', [sheet.name for sheet in sheets]
+		print '------------------------------------------------------------------------------------------'
+
+		for sheet in sheets:
+			"""
+			column 0 of every sheet is supposed to containe indexes of the stores.
+			"""
 			column_0_values = sheet.col_values(colx=0)
 
-			start_tag = 1
+			"""
+			column 0 indexes should always start from 1 and not 0. Also these indexes should always be in the first row of their row-range.
+			To fetch store data from sheets we follow following logic:
+			* Get Row index of start_tag
+			* Get Row index of end_tag. (end_tag = start_tag + 1)
+			* All the data between these rows will be of current store.
+			"""
+			start_tag = 1 
 			end_tag = start_tag + 1
-			print "Processing sheet: ", sheet.name
 
 			while(start_tag in column_0_values):
-				user_count = user_count + 1
+				store_count = store_count + 1
 				start_index = column_0_values.index(start_tag)
 				if end_tag in column_0_values:
 					end_index = column_0_values.index(end_tag)
+					"""
+						This API will fetch all the rows between rang(start_index, end_index) and process the data.
+						Will create users and their respective stores.
+					"""
+					create_store(sheet, start_index, end_index, store_count, start_tag )
 					start_tag = end_tag
 					end_tag = start_tag + 1
-					create_store(sheet, start_index, end_index, user_count )
 				else:
-					create_store(sheet, start_index, sheet.nrows, user_count)
+					create_store(sheet, start_index, sheet.nrows, store_count, start_tag)
 					break
 
-	passbook.save('xls/passwords.xls')
+	passbook.save(PASSWORD_FILE_PATH)
 
 
 		
