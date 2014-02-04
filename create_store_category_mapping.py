937e88925df23a16063425272e1c2e8882c5877f
saumishr <saumishr@www.domain1.com>
2014-02-04 18:50:15 +0530

    added log for better exception

--- create_store_category_mapping.py
+++ create_store_category_mapping.py
@@ -56,7 +56,7 @@ if __name__ == "__main__":
 				if kw:
 					keyword_id = Keyword.objects.get_or_create(title=kw)[0].id
 					blog_post.keywords.add(AssignedKeyword(keyword_id=keyword_id))
-		except e:
+		except Exception as e:
 			print e
 			print "Encountered error with:", store_name
 			pass
